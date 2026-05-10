6e752cc93b67a519cb74a90058b8b21dba65bfd4
Basil <haerrbas@students.zhaw.ch>
2026-05-10 16:46:11 +0200

    fix: patch _headers + functions config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      fallback: '200.html'
-    })
+    adapter: adapter()
   }
 };
 
